d0d876d465481f7dec6040b50e5448021cf7cacc
Joshua Baker <thepotatoprogrammer@gmail.com>
2022-09-19 19:36:27 -0500

    Initial Commit

---
+++ Swift2Tetris/Swift2TetrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift2TetrisApp.swift
+//  Swift2Tetris
+//
+//  Created by Joshua Baker on 9/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift2TetrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
